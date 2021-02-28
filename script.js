696cc6d673ba4afdd4c47bdbff0cd7fab89f138a
Glitch (hello-webpage) <none>
2021-02-28 22:00:27 +0000

    🎾🏘 Checkpoint ./script.js:29168728/139

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0; 
 var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5; 
 function startGame(){
     //initialize game variables
     progress = 0;
